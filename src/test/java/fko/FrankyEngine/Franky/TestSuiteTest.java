5935af49c776c2d064d564652fa82ad1ea47f9c7
Frank Kopp <frank@familie-kopp.de>
2018-12-16 14:03:53 -0800

    no message

--- src/test/java/fko/FrankyEngine/Franky/TestSuiteTest.java
+++ src/test/java/fko/FrankyEngine/Franky/TestSuiteTest.java
@@ -40,14 +40,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * TestSuiteTest
- *
+ * <p>
  * https://www.chessprogramming.org/Test-Positions#Test_Suites
  * http://www.bergbomconsulting.se/chess/testsuites.html
- *
+ * <p>
  * TODO: Implement test set testing
  * TODO: Implement reading test sets from file - Extended Position Description (EPD)
  * https://www.chessprogramming.org/Extended_Position_Description
- *
  */
 public class TestSuiteTest {
 
@@ -61,7 +60,7 @@ public class TestSuiteTest {
   }
 
   @AfterEach
-  void tearDown()  {
+  void tearDown() {
   }
 
   @Test
@@ -77,16 +76,21 @@ public class TestSuiteTest {
 
   @Test
   void startOneTest() {
-    //    testSuite.startOneTest("r1q1r1k1/1p3pp1/2p1bn1p/p3N3/2P2P2/P1Q1R3/1PB3PP/4R1K1 w - - bm h3; " +
-    //                           "id \"Crafty Test Pos.23\"; c0 \"GK/DB Philadelphia 1996, Game 4, move" +
+    //    testSuite.startOneTest("r1q1r1k1/1p3pp1/2p1bn1p/p3N3/2P2P2/P1Q1R3/1PB3PP/4R1K1 w - - bm
+    //    h3; " +
+    //                           "id \"Crafty Test Pos.23\"; c0 \"GK/DB Philadelphia 1996, Game
+    //                           4, move" +
     //                           " 21W (h3)\";", 5000);
-    //testSuite.startOneTest("1k1r3r/pp3pp1/1np1p1p1/4q3/1P1P4/5N2/P1P1RPPP/4R1K1 w - - bm Nxe5;", 5000);
-    testSuite.startOneTest("r1bqkb1r/ppp1p1pp/1nnpp3/8/2PP4/5N2/PP3PPP/RNBQKB1R w KQkq - bm Ng5", 5000);
+    //testSuite.startOneTest("1k1r3r/pp3pp1/1np1p1p1/4q3/1P1P4/5N2/P1P1RPPP/4R1K1 w - - bm Nxe5;
+    // ", 5000);
+    testSuite.startOneTest("r1bqkb1r/ppp1p1pp/1nnpp3/8/2PP4/5N2/PP3PPP/RNBQKB1R w KQkq - bm Ng5",
+                           5000);
   }
 
   @Test
   void startOneMateTest() {
     assertTrue(testSuite.startOneTest("8/8/8/8/8/3K4/R7/5k2 w - - dm 4;", 5000));
-    assertTrue(testSuite.startOneTest("8/8/8/8/4K3/8/R7/4k3 w - - dm 5;",5000));
+    assertTrue(testSuite.startOneTest("8/8/8/8/4K3/8/R7/4k3 w - - dm 5;", 5000));
   }
+
 }
