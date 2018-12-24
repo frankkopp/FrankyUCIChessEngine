33562abdb4a1e7e6e707d0fd279dedc50e0170d5
Frank Kopp <frank@familie-kopp.de>
2018-12-24 01:04:40 +0100

    Added OpeningBook from Chessly and migrated it to Franky

---
+++ src/main/java/fko/FrankyEngine/openingbook/Openingbook_Configuration.java
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Frank Kopp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package fko.FrankyEngine.openingbook;
+
+import fko.FrankyEngine.openingbook.OpeningBookImpl.Mode;
+
+/**
+ * @author fkopp
+ */
+public class Openingbook_Configuration {
+
+    /**
+     * If set to true this object will produce info output to System.out
+     */
+    public boolean VERBOSE = true;
+
+    /**
+     * If set to true will ignore an existing cache file and recreate it
+     */
+    public boolean FORCE_CREATE = false;
+
+    /** default value for folder to books */
+    public String               _folderPath    = "/book/";
+    /** default value for serialization folder */
+    public String               _serPath       = "./var/book_cache";
+    /** default opening book */
+    public String               _fileNamePlain = "Test_PGN/perle.pgn";
+    /** default mode */
+    public Mode _mode          = Mode.PGN;
+
+    /** Creates opening book configuration object */
+    public Openingbook_Configuration() {}
+}
---
+++ src/main/java/fko/FrankyEngine/util/HelperTools.java
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Frank Kopp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package fko.FrankyEngine.util;
+
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * This class just provides some helper utilities and cannot be instanciated.
+ */
+public final class HelperTools {
+
+    private final static Format digitFormat = new DecimalFormat("00");
+    private final static Format milliFormat = new DecimalFormat("000");
+
+    private HelperTools() {}
+
+    /**
+     * get a MByte String from a byte input
+     * @param digit
+     * @return String
+     */
+    public static String getDigit(long digit) {
+        Locale.setDefault(new Locale("de", "DE"));
+        NumberFormat f = NumberFormat.getInstance();
+        if (f instanceof DecimalFormat) {
+            f.setMaximumFractionDigits(1);
+        }
+        return f.format(digit);
+    }
+
+    /**
+     * get a MByte String from a byte input
+     * @param bytes
+     * @return String
+     */
+    public static String getMBytes(long bytes) {
+        double d = (Long.valueOf(bytes)).doubleValue() / (1024.0 * 1024.0);
+        NumberFormat f = NumberFormat.getInstance();
+        if (f instanceof DecimalFormat) {
+            f.setMaximumFractionDigits(1);
+        }
+        return f.format(d);
+    }
+
+    /**
+     * format a given time into 00:00:00
+     * @param time
+     * @param milliseconds
+     * @return formatted string
+     */
+    public static String formatTime(long time, boolean milliseconds) {
+        StringBuilder sb = new StringBuilder(digitFormat.format((time / 1000 / 60 / 60)));
+        sb.append(':');
+        sb.append(digitFormat.format((time / 1000 / 60) % 60));
+        sb.append(':');
+        sb.append(digitFormat.format((time / 1000) % 60));
+        if (milliseconds) {
+            sb.append('.');
+            sb.append(milliFormat.format(time % 1000));
+        }
+        return sb.toString();
+    }
+}
