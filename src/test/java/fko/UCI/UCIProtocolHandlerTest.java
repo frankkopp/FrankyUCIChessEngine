0d3bf21b2d4d0ea4543c5d5c1c1e95c5dc01e50b
Frank Kopp <frank@familie-kopp.de>
2018-12-28 10:43:47 +0100

    faster testing

--- src/test/java/fko/UCI/UCIProtocolHandlerTest.java
+++ src/test/java/fko/UCI/UCIProtocolHandlerTest.java
@@ -406,17 +406,16 @@ class UCIProtocolHandlerTest {
     assertEquals("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2",
                  engine.getPosition().toFENString());
 
-    toHandlerPrinter.println("go ponder wtime 300000 btime 300000");
+    toHandlerPrinter.println("go ponder wtime 3000 btime 3000");
     semaphore.tryAcquire(2000, TimeUnit.MILLISECONDS);
 
     assertTrue(engine.getSearchMode().isPonder());
     assertTrue(engine.isSearching());
 
-    Thread.sleep(500);
+    Thread.sleep(100);
 
     toHandlerPrinter.println("ponderhit");
     semaphore.tryAcquire(2000, TimeUnit.MILLISECONDS);
-
 
     // clear buffer
     while (fromHandlerReader.ready()) {
