59d9621b0ec363850334f1139d775ec3f8c99f37
Frank Kopp <frank@familie-kopp.de>
2019-01-04 09:00:05 +0100

    Code cleanup

--- src/test/java/fko/FrankyEngine/Franky/TestTranspositionTable.java
+++ src/test/java/fko/FrankyEngine/Franky/TestTranspositionTable.java
@@ -101,64 +101,6 @@ public class TestTranspositionTable {
     System.out.println();
   }
 
-  /**
-   *  different randoms influence tt collisions greatly
-   *  tried to find a good seed here
-   */
-  @Test
-  @Disabled
-  public void findSeed() {
-
-    // depth 8
-    // best: 2157, coll 4736, idx 4060
-
-    //    Start COLLISION Test for Seed 61
-    //    TT Objects: 3.464.890 (6.100.805)
-    //    TT Collisions: 1.878.548 Updates: 987.937
-    //    Best Collisions 1.696.168 with seed 40
-
-    int bestSeed = 0;
-    long bestCollisions = 1696168;
-
-    for (int seed = 2157; seed < Integer.MAX_VALUE; seed++) {
-
-      Position.setZobristRandoms(seed);
-      engine = new FrankyEngine();
-      search = ((FrankyEngine) engine).getSearch();
-
-      final int depth = 10;
-      System.out.printf("Start COLLISION Test for Seed %,d %n", seed);
-
-      Position position = new Position();
-
-      SearchMode searchMode =
-        new SearchMode(0, 0, 0, 0, 0, 0, 0, depth, 0, null, false, true, false);
-
-      search.startSearch(position, searchMode);
-
-      search.waitWhileSearching();
-
-      LOG.info("Best Move: {} Value: {} Ponder {}",
-               Move.toSimpleString(search.getLastSearchResult().bestMove),
-               search.getLastSearchResult().resultValue / 100f,
-               Move.toSimpleString(search.getLastSearchResult().ponderMove));
-
-      System.out.printf("TT Objects: %,d (%,d) %n",
-                        search.getTranspositionTable().getNumberOfEntries(),
-                        search.getTranspositionTable().getMaxEntries());
-      System.out.printf("TT Collisions: %,d Updates: %,d %n",
-                        search.getTranspositionTable().getNumberOfCollisions(),
-                        search.getTranspositionTable().getNumberOfUpdates());
-
-      if (search.getTranspositionTable().getNumberOfCollisions() < bestCollisions) {
-        bestSeed = seed;
-        bestCollisions = search.getTranspositionTable().getNumberOfCollisions();
-      }
-      System.out.printf("Best Collisions %,d with seed %d %n", bestCollisions, bestSeed);
-      System.out.println();
-    }
-  }
-
 
   /**
    * General test of TT usage
@@ -216,6 +158,64 @@ public class TestTranspositionTable {
       System.out.format("TT Size (config): %dMB = %dMB real size - Nodes: %d%n", i, hashAllocation,
                         tt.getMaxEntries());
       tt = null;
+    }
+  }
+
+  /**
+   *  different randoms influence tt collisions greatly
+   *  tried to find a good seed here
+   */
+  @Test
+  @Disabled
+  public void findSeed() {
+
+    // depth 8
+    // best: 2157, coll 4736, idx 4060
+
+    //    Start COLLISION Test for Seed 61
+    //    TT Objects: 3.464.890 (6.100.805)
+    //    TT Collisions: 1.878.548 Updates: 987.937
+    //    Best Collisions 1.696.168 with seed 40
+
+    int bestSeed = 0;
+    long bestCollisions = 1696168;
+
+    for (int seed = 2157; seed < Integer.MAX_VALUE; seed++) {
+
+      Position.setZobristRandoms(seed);
+      engine = new FrankyEngine();
+      search = ((FrankyEngine) engine).getSearch();
+
+      final int depth = 10;
+      System.out.printf("Start COLLISION Test for Seed %,d %n", seed);
+
+      Position position = new Position();
+
+      SearchMode searchMode =
+        new SearchMode(0, 0, 0, 0, 0, 0, 0, depth, 0, null, false, true, false);
+
+      search.startSearch(position, searchMode);
+
+      search.waitWhileSearching();
+
+      LOG.info("Best Move: {} Value: {} Ponder {}",
+               Move.toSimpleString(search.getLastSearchResult().bestMove),
+               search.getLastSearchResult().resultValue / 100f,
+               Move.toSimpleString(search.getLastSearchResult().ponderMove));
+
+      System.out.printf("TT Objects: %,d (%,d) %n",
+                        search.getTranspositionTable().getNumberOfEntries(),
+                        search.getTranspositionTable().getMaxEntries());
+      System.out.printf("TT Collisions: %,d Updates: %,d %n",
+                        search.getTranspositionTable().getNumberOfCollisions(),
+                        search.getTranspositionTable().getNumberOfUpdates());
+
+      if (search.getTranspositionTable().getNumberOfCollisions() < bestCollisions) {
+        bestSeed = seed;
+        bestCollisions = search.getTranspositionTable().getNumberOfCollisions();
+      }
+      System.out.printf("Best Collisions %,d with seed %d %n", bestCollisions, bestSeed);
+      System.out.println();
     }
   }
 
