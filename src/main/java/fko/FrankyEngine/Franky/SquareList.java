d8c8d05ae9a7f41b136cc5ec6c9138be1d8f2e73
Frank Kopp <frank@familie-kopp.de>
2019-02-25 18:03:33 +0100

    CleanUp

--- src/main/java/fko/FrankyEngine/Franky/SquareList.java
+++ src/main/java/fko/FrankyEngine/Franky/SquareList.java
@@ -30,112 +30,107 @@ package fko.FrankyEngine.Franky;
  */
 public class SquareList {
 
-    private static final int MAX_SIZE = 65;
+  private static final int MAX_SIZE = 65;
 
-    private final Square[] elements = new Square[MAX_SIZE];
+  private final Square[] elements = new Square[MAX_SIZE];
 
-    private int size = 0;
+  private int size = 0;
 
-    /**
-     * @return the size
+  /**
+   * @return the size
+   */
+  public int size() {
+    return size;
+  }
+
+  /**
+   * @param i
+   * @return the element at index i
+   */
+  public Square get(int i) {
+    if (i >= size) throw new ArrayIndexOutOfBoundsException();
+    return this.elements[i];
+  }
+
+  /**
+   * Adds the given square to the beginning of the list
+   * @param square
+   */
+  public void add(Square square) {
+    if (size >= MAX_SIZE - 1)
+      throw new ArrayStoreException("SquareList is full");
+
+    /*
+     * we need to keep the order stable because when
+     * iterating over the list and removing and adding
+     * the same element should not change the list at all!
+     * (e.g. when makeMove/undoMove happens)
      */
-    public int size() {
-        return size;
-    }
 
-    /**
-     * @param i
-     * @return the element at index i
-     */
-    public Square get(int i) {
-        if (i>=size) throw new ArrayIndexOutOfBoundsException();
-        return this.elements[i];
-    }
+    final int ordinal = square.ordinal();
 
-    /**
-     * Adds the given square to the beginning of the list
-     * @param square
-     */
-    public void add(Square square) {
-        if (size >= MAX_SIZE-1)
-            throw new ArrayStoreException("SquareList is full");
-
-        /*
-         * we need to keep the order stable because when
-         * iterating over the list and removing and adding
-         * the same element should not change the list at all!
-         * (e.g. when makeMove/undoMove happens)
-         */
-
-        final int ordinal = square.ordinal();
-
-        // go backward and move all elements one place to the right
-        // when the right place is found insert the new element.
-        for (int i = size-1; i >= 0; i--) {
-            if (ordinal > elements[i].ordinal()) {
-                elements[i+1] = square;
-                size++;
-                return;
-            }
-            elements[i+1] = elements[i]; // move the element to the right
-        }
-      // we did not find a place -> element seams to be first in order
-        // add it to place 0 as this is now free
-        elements[0] = square;
+    // go backward and move all elements one place to the right
+    // when the right place is found insert the new element.
+    for (int i = size - 1; i >= 0; i--) {
+      if (ordinal > elements[i].ordinal()) {
+        elements[i + 1] = square;
         size++;
+        return;
+      }
+      elements[i + 1] = elements[i]; // move the element to the right
     }
+    // we did not find a place -> element seams to be first in order
+    // add it to place 0 as this is now free
+    elements[0] = square;
+    size++;
+  }
 
-    /**
-     * Remove the given element from the list.
-     * Does nothing if element is not in the list.
-     * @param square
+  /**
+   * Remove the given element from the list.
+   * Does nothing if element is not in the list.
+   * @param square
+   */
+  public void remove(Square square) {
+    /*
+     * Go over the array from left to right until you found the element.
+     * copy the next element to the elements place (overwriting it).
      */
-    public void remove(Square square) {
-
-        /*
-         * Go over the array from left to right until you found the element.
-         * copy the next element to the elements place (overwriting it).
-         * Done.
-         */
-        Square toBeRemoved = square;
-        for (int i=0; i<size; i++) {
-            if (toBeRemoved == elements[i]) { // hit
-                elements[i] = elements[i+1]; // overwrite the element with the one from the right
-                toBeRemoved = elements[i+1]; // now the next to the right can be removed
-            }
-        }
-        // only reduce if we found our element in the list
-        if (toBeRemoved!=square)
-            size--;
-
+    Square toBeRemoved = square;
+    for (int i = 0; i < size; i++) {
+      if (toBeRemoved == elements[i]) { // hit
+        elements[i] = elements[i + 1]; // overwrite the element with the one from the right
+        toBeRemoved = elements[i + 1]; // now the next to the right can be removed
+      }
     }
+    // only reduce if we found our element in the list
+    if (toBeRemoved != square) size--;
+  }
 
-    /**
-     * @return true if size == 0
-     */
-    public boolean isEmpty() {
-        return size == 0;
+  /**
+   * @return true if size == 0
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder s = new StringBuilder("[" + size + "] ");
+    for (int i = 0; i < size; i++) {
+      s.append(elements[i]).append(" ");
     }
+    return s.toString();
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder s = new StringBuilder("[" + size + "] ");
-        for (int i = 0; i < size; i++) {
-            s.append(elements[i]).append(" ");
-        }
-        return s.toString();
-    }
-
-    /**
-     * Returns a deep copy of the list
-     */
-    @Override
-    public SquareList clone() {
-        SquareList clone = new SquareList();
-        System.arraycopy(this.elements, 0, clone.elements, 0, elements.length);
-        clone.size = this.size;
-        return clone;
-    }
-
+  /**
+   * Returns a deep copy of the list
+   */
+  @Override
+  public SquareList clone() {
+    SquareList clone = new SquareList();
+    System.arraycopy(this.elements, 0, clone.elements, 0, elements.length);
+    clone.size = this.size;
+    return clone;
+  }
 
 }
